df441a0b1ae2589652d5a2494e1fccc088fd65a1
Trinity <98069668+tlklein@users.noreply.github.com>
2024-07-20 21:46:42 -0500

    Change CSS Again

--- frontend/app/(dashboard)/dashboard/page.tsx
+++ frontend/app/(dashboard)/dashboard/page.tsx
@@ -33,8 +33,9 @@ export default function page() {
       <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
-            Hi, Welcome Back!
+            Hi, Welcome Back! 
           </h2>
+          <h3>Choose your data & start your race!</h3>
           {/*
           <div className="hidden items-center space-x-2 md:flex">
             <CalendarDateRangePicker />
