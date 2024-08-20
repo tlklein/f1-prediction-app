5c0f04b8262125fd255157b0123faadfad2467d9
Trinity <98069668+tlklein@users.noreply.github.com>
2024-08-20 15:22:42 -0500

    Update data

--- frontend/constants/data.ts
+++ frontend/constants/data.ts
@@ -1,6 +1,4 @@
 import { NavItem } from '@/types';
-
-
 export type Drivers = {
   id: number;
   race_number: number;
@@ -535,26 +533,6 @@ export const teams: Teams[] = [
     power_unit: 'Ferrari'
   }
 ];
-
-
-export type Employee = {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone: string;
-  gender: string;
-  date_of_birth: string; // Consider using a proper date type if possible
-  street: string;
-  city: string;
-  state: string;
-  country: string;
-  zipcode: string;
-  longitude?: number; // Optional field
-  latitude?: number; // Optional field
-  job: string;
-  profile_picture?: string | null; // Profile picture can be a string (URL) or null (if no picture)
-};
 
 export const navItems: NavItem[] = [
   {
